2e36f9a0ebca49ed1099d7b56e1c3e4d334f8ece
zhouyu <zhouyu002@obsbot.com>
2026-02-12 11:55:31 +0800

    chore: Init Flutter Project

---
+++ android/app/src/main/kotlin/com/example/emby_viwer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.emby_viwer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Flutter smoke test', (WidgetTester tester) async {});
+}
